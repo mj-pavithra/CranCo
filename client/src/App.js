0c7836a8a14d815bb8fd1eb271dd4b33883c2d5f
Anjuna Serasingha <92202036+anjuna0305@users.noreply.github.com>
2023-07-17 00:35:29 +0530

    primary button component created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Btn from './components/Btn';
 import HelloWorld from './components/HelloWorld';
 
 function App() {
   return (
     <div className="App">
       <HelloWorld/>
+      <Btn  buttonText="Click me!"/>
     </div>
   );
 }
