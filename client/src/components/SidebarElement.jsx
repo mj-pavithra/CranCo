3f0ef41ff2649de59e29711714cbc2c5f0213fec
Anjuna Serasingha <92202036+anjuna0305@users.noreply.github.com>
2023-07-17 18:46:15 +0530

    react router dom added

---
+++ client/src/components/SidebarElement.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SidebarElementCss from './SidebarElement.module.css';
+
+const SidebarElement = () => {
+  return (
+    <div className={SidebarElementCss.sidebar_element_div}>SidebarElement</div>
+  )
+}
+
+export default SidebarElement
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <div>this is HomePage</div>
+    )
+}
+
+export default HomePage
