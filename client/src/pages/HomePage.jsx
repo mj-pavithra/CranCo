5a918f54f829acaef8c0bbc7e157435c5385af87
Anjuna Serasingha <92202036+anjuna0305@users.noreply.github.com>
2023-07-22 12:37:13 +0530

    basic page structure added

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -5,6 +5,22 @@ const HomePage = () => {
     return (
         <div>
             <NavigationBar />
+            <body>
+                <div >
+                    <div className='div-1'>
+                        <div className='div-left'></div>
+                        <div className='div-middle'>
+                            <div className='post-div-temp'></div>
+                            <div className='post-div-temp'></div>
+                            <div className='post-div-temp'></div>
+                            <div className='post-div-temp'></div>
+                            <div className='post-div-temp'></div>
+                            <div className='post-div-temp'></div>
+                        </div>
+                        <div className='div-right'></div>
+                    </div>
+                </div>
+            </body>
         </div>
     )
 }
