f5e80102a4cf2374c9d99e645c24c19b9045b1c9
awed-00 <92774423+AmayaNW@users.noreply.github.com>
2023-07-24 15:50:43 +0530

    Changed

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,18 +1,39 @@
 import React from "react";
 import NavigationBar from "../components/NavigationBar";
-import Post from '../components/Post'
+import Post from "../components/Post";
+import UserSidebar from "../components/UserSidebar";
+import { useEffect, useState } from "react";
 
 const HomePage = () => {
+  const [userData, setUserData] = useState({
+    image: "",
+    username: "",
+  });
+  const fetchUserData = () => {
+    // backend to fetch user data
+    setUserData({
+      image: "../public/girl.png",
+      username: "Kaveesha Gunawardana",
+    });
+  };
+
+  // fetch user data when component mounts
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
   return (
     <>
       <NavigationBar />
       <body>
-        <div className='div-1'>
-          <div className='div-left'></div>
-          <div className='div-middle'>
+        <div className="div-1">
+          <div className="div-left">
+            <UserSidebar image={userData.image} username={userData.username} />
+          </div>
+          <div className="div-middle">
             <Post />
           </div>
-          <div className='div-right'></div>
+          <div className="div-right"></div>
         </div>
       </body>
     </>
