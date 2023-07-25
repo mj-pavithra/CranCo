ced5122ed47678e83f53af5e9f734727c30a72e5
MJ Pavithra <mjpavithra6520@gmail.com>
2023-07-25 14:39:57 +0530

    Revert "Page_component"
    This reverts commit 9b6f5db9c7ba97fc497763fe1725cb4ba96f2622.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,17 @@
-import './App.css';
+import "./App.css";
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HelloWorld from './components/HelloWorld';
-import HomePage from './pages/HomePage';
-import Pages from './components/Pages';
+import LoginPage from "./pages/LoginPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/'>
-            <Route index element={<HelloWorld />} />
-            <Route path='/homepage' element={<HomePage />} />
-            <Route path='/feed' element={<Pages />} />
+          <Route path="/">
+            <Route path="/homepage" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,12 +1,43 @@
-import React from 'react'
-import NavigationBar from '../components/NavigationBar'
+import React from "react";
+import NavigationBar from "../components/NavigationBar";
+import Post from "../components/Post";
+import UserSidebar from "../components/UserSidebar";
+import { useEffect, useState } from "react";
 
 const HomePage = () => {
-    return (
-        <div>
-            <NavigationBar />
-        </div>
-    )
-}
+  const [userData, setUserData] = useState({
+    image: "",
+    username: "",
+  });
+  const fetchUserData = () => {
+    // backend to fetch user data
+    setUserData({
+      image: "../public/girl.png",
+      username: "Kaveesha Gunawardana",
+    });
+  };
 
-export default HomePage
+  // fetch user data when component mounts
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
+  return (
+    <>
+      <NavigationBar />
+      <body>
+        <div className="div-1">
+          <div className="div-left">
+            <UserSidebar image={userData.image} username={userData.username} />
+          </div>
+          <div className="div-middle">
+            <Post />
+          </div>
+          <div className="div-right"></div>
+        </div>
+      </body>
+    </>
+  );
+};
+
+export default HomePage;
