b43cc009abfb7a1374f487d38329057ad001a618
MJ Pavithra <mjpavithra6520@gmail.com>
2023-07-25 13:59:02 +0530

    page complete

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import NavigationBar from "../components/NavigationBar";
 import Post from "../components/Post";
 import UserSidebar from "../components/UserSidebar";
+import Pages from "../components/Pages";
 
 const HomePage = () => {
   const [userData, setUserData] = useState({
@@ -32,7 +33,9 @@ const HomePage = () => {
           <div className="div-middle">
             <Post />
           </div>
-          <div className="div-right"></div>
+          <div className="div-right">
+            <Pages />
+          </div>
         </div>
       </body>
     </>
