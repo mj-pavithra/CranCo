a7ea1c4e0d3103790b9def5489d09a0862c33941
MJ Pavithra <mjpavithra6520@gmail.com>
2023-07-29 04:13:04 +0530

    Plwan tharamak bugs fix kala (maga aruna mona hari thibboth kiyanna:-)

--- client/src/Components/eachitem.jsx
+++ client/src/Components/eachitem.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import "../css/eachItem.css";
 import Btn from "./Btn";
 
-const EachItem = ({itemName, dis}) => {
-  return (
-    <div className="each-item">
-      <img className="item-img" src="/girl.png" alt="profile" />
-      <div className="item-text">
-        <h3 className="item-name">{itemName}</h3>
-        <h4 className="item-dis">Colombo</h4>
+const EachItem = ({ itemName, dis, img , action}) => {
+    return (
+      <div className="each-item">
+        <img className="item-img" src={img} alt="profile" />
+        <div className="item-text">
+          <h3 className="item-name">{itemName}</h3>
+          <h4 className="item-dis">{dis}</h4>
+        </div>
+        <Btn buttonText={action} />
       </div>
-      <Btn buttonText="Confirm" />
-    </div>
-  );
-};
+    );
+  };
 
 export default EachItem;
--- client/src/pages/MidMaxBlock.jsx
+++ client/src/pages/MidMaxBlock.jsx
@@ -1,8 +1,7 @@
 import {useState} from "react";
 import EachItem from "../components/EachItem";
-import Btn from "../components/Btn";
 
-function MidMaxBlock({ rowTitle, items }) {
+function MidMaxBlock({ rowTitle, items , image, action, dis }) {
   // Define showGrid state and setShowGrid state setter
   const [showGrid, setShowGrid] = useState(false);
 
@@ -10,11 +9,9 @@ function MidMaxBlock({ rowTitle, items }) {
     
       <div className="mid-max-block">
         <h1 className="row-title">{rowTitle}</h1>
-        <div
-          className={showGrid ? 'grid-container' : 'row-container'}
-        >
+        <div className={showGrid ? 'grid-container' : 'row-container'}>
           {items.map((item, index) => (
-            <EachItem key={index} itemName={item} />
+            <EachItem key={index} itemName={item} dis={dis} img={image} action={action}/>
           ))}
         </div>
         <button className="see-more-btn" onClick={() => setShowGrid(!showGrid)}>
