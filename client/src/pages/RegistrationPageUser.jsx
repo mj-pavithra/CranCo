c3a0c84b1dc049aaf154f875baa2ef54a9bfe597
Anjuna Serasingha <92202036+anjuna0305@users.noreply.github.com>
2023-07-29 14:54:17 +0530

    bug fixed

--- client/src/pages/RegistrationPageUser.jsx
+++ client/src/pages/RegistrationPageUser.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import LoginInput from '../components/LoginInput'
 import Checkbox from '../components/Checkbox'
 import Btn from '../components/Btn'
