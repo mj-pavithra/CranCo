f543d64f35e01660233b82cda18e3271405c3eb4
MJ Pavithra <mjpavithra6520@gmail.com>
2023-07-31 23:40:16 +0530

    "

--- client/src/Components/ProfileCard.jsx
+++ client/src/Components/ProfileCard.jsx
@@ -1,52 +1,29 @@
 import React from "react";
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import Avatar from "@mui/material/Avatar";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
+import "../css/profileCard.css";
 
-const ProfileCard = () => {
-  return (
-    <Card>
-      {/* Profile cover photo in background */}
-      <div
-        style={{
-          backgroundImage: "url(profile_cover_photo_url)",
-          height: "200px",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      ></div>
 
-      <CardHeader
-        avatar={<Avatar src="profile_picture_url" alt="User Profile" />}
-        title="User Name"
-        subheader="Location"
-      />
-
-      <CardContent>
-        {/* Update profile picture button */}
-        <Button variant="contained">Update Profile Picture</Button>
-
-        <Typography variant="body1">
-          This is the content of the profile card. It can include any additional information about the user.
-        </Typography>
-
-        {/* Tiny images of friends profile pictures */}
-        <div style={{ display: "flex", gap: "5px", marginTop: "10px" }}>
-          <Avatar src="friend_1_profile_picture_url" alt="Friend 1" />
-          <Avatar src="friend_2_profile_picture_url" alt="Friend 2" />
-          <Avatar src="friend_3_profile_picture_url" alt="Friend 3" />
-          {/* Add more friend profile pictures here */}
+  
+    return (
+   <div className="profileCard">
+    <div className="profileCardtop">
+        <img className="coverPhoto" alt="/assets/alt-image.jpg">{coverPhoto}</img>
+    </div>
+    <div className="profileCardmiddle">
+        <img className="profilePhoto">{profilePhoto}</img>
+        <img className="editDP" alt="/assets/alt-image.jpg">{editDP}</img>
+    </div>
+    <div className="profileCardbottom">
+        <h1 className="profileName">{profileName}</h1>
+        <div classname="pages">
+            <img className="likedPage1" alt="/assets/alt-image.jpg">{likedPage1}</img>
+            <img className="likedPage2" alt="/assets/alt-image.jpg">{likedPage2}</img>
+            <img className="likedPage3" alt="/assets/alt-image.jpg">{likedPage3}</img>
+            <h3 className="likedPageCount">{likedPageCount}</h3>
         </div>
-
-        {/* Add friend button */}
-        <Button variant="contained" style={{ marginTop: "10px" }}>
-          Add Friend
-        </Button>
-      </CardContent>
-    </Card>
+        <h3 className="RalionshipState"></h3>
+        <button className="addFriend">{addFriend}</button>
+    </div>
+   </div>
   );
 };
 
