43669cd190680dfa8b94ccba61f7a8aa610a0807
Anjuna Serasingha <92202036+anjuna0305@users.noreply.github.com>
2023-08-03 14:37:32 +0530

    at the middle of work

--- client/src/Components/Pages.jsx
+++ client/src/Components/Pages.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../css/pages.css";
+// import "../css/pages.css";
 import { useNavigate } from "react-router-dom";
 
 const Pages = () => {
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,57 +1,3 @@
-<<<<<<< HEAD
-import React, { useEffect, useState } from "react";
-import NavigationBar from "../components/NavigationBar";
-import Post from "../components/Post";
-import UserSidebar from "../components/UserSidebar";
-import RightColSubContainer from "../components/rightColSubContainer";
-
-const HomePage = () => {
-  const [userData, setUserData] = useState({
-    image: "",
-    username: "",
-  });
-  const fetchUserData = () => {
-    // backend to fetch user data
-    setUserData({
-      image: "/girl.png",
-      username: "Kaveesha Gunawardana",
-    });
-  };
-
- 
-    const pagesData = [
-      { name: 'Page 1', imgUrl: '/assets/audir8.jpeg' },
-      { name: 'Page 2', imgUrl: '/assets/audir8.jpeg' },
-      { name: 'Page 3', imgUrl: '/assets/audir8.jpeg' },
-      { name: 'Page 4', imgUrl: '/assets/audir8.jpeg' },
-      { name: 'Page 5', imgUrl: '/assets/audir8.jpeg' },
-      { name: 'Page 6', imgUrl: '/assets/audir8.jpeg' },
-      
-    ];
-
-  // fetch user data when component mounts
-  useEffect(() => {
-    fetchUserData();
-  }, []);
-
-  return (
-    <>
-      <NavigationBar />
-      <body>
-        <div className="div-1">
-          <div className="div-left">
-            <UserSidebar image={userData.image} username={userData.username} />
-          </div>
-          <div className="div-middle">
-            <Post />
-          </div>
-          <div className="div-right">
-          <RightColSubContainer containerTitle="Pages" Items={pagesData} btnTxt="Create Page" />
-            <RightColSubContainer containerTitle="Trending" Items={pagesData} btnTxt= ""  />
-          </div>
-        </div>
-      </body>
-=======
 // HomePage.jsx
 import React from "react";
 import Post from "../components/Post"; // Import any other specific components for the Home page
@@ -63,7 +9,6 @@ const HomePage = () => {
       <MainContainer>
         <Post />
       </MainContainer>
->>>>>>> b050524d184a12b80d3adf7ca35963e6adbdf326
     </>
   );
 };
