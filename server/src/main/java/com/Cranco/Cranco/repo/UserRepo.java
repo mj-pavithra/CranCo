a8062399ada52d5e04e858188ac44d96f4d73787
Suchith Sandunika <esuchith@gmail.com>
2023-08-03 13:15:07 +0530

    edited some files

--- server/src/main/java/com/Cranco/Cranco/repo/UserRepo.java
+++ server/src/main/java/com/Cranco/Cranco/repo/UserRepo.java
@@ -4,4 +4,5 @@ import com.Cranco.Cranco.model.User;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 
 public interface UserRepo extends Neo4jRepository<User,Integer> {
+    boolean exitById(String userId);
 }
