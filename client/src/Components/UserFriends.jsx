aad4037727d4db8d46aef29ca14caacb92b5cbf2
MJ Pavithra <mjpavithra6520@gmail.com>
2023-08-04 00:55:26 +0530

    user friend component created

---
+++ client/src/Components/UserFriends.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "../css/UserFriends.css";
+
+
+const UserFriends = ({ ProfilePicture, userName, pageImage1, pageImage2 , pageCount }) => {
+    const onError = (originalImg) => {
+        const altImg = "/assets/alt-image.jpeg";
+      if (originalImg === null || originalImg === "") {
+        return altImg;
+      }
+      return originalImg;
+    };
+
+    return (
+        <div className="topContainer">
+            <div className="proPicContainer">
+                <img className="proPic" src={onError(ProfilePicture)} alt="logo" />
+            </div>
+            <div className="detailContainer">
+                <p className="userName">{userName}</p>
+                <div className="pageContainer">
+                    <img className="pageImg " src={onError(pageImage1)} alt="logo" />
+                    <img className="pageImg secondImg" src={onError(pageImage2)} alt="logo" />
+                    <p className="pageCount">{pageCount} Pages</p>
+                </div>
+            </div>
+        </div>
+    );
+    
+};
+
+export default UserFriends;
