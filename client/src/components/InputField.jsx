25e2be7a17c3a412a2ce6ee3516a0cf8f7bdde3f
Kaveesha Gunawardana <92759072+Kaveesha293@users.noreply.github.com>
2023-08-16 03:03:20 +0530

    Kaveesha_Add new soul update

--- client/src/components/InputField.jsx
+++ client/src/components/InputField.jsx
@@ -1,10 +1,10 @@
 import "../css/InputField.css"
 
-const InputField = ({topic, placeholder}) => {
+const InputField = ({ topic, placeholder, flexDirection, alignContent, width }) => {
     return ( 
-        <div className="inputFieldContainer">
+        <div className={`inputFieldContainer ${flexDirection} ${alignContent}`}>
             <div className="topic">{topic}</div>
-            <input type="text" placeholder={placeholder} className="inputField"/>
+            <input type="text" placeholder={placeholder} className={`inputField`} style={{ width: width }}/>
         </div>
      );
 }
