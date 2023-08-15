88a106b6e4bf9ad819566695a2213a9609d9808e
Anjuna Serasingha <92202036+anjuna0305@users.noreply.github.com>
2023-08-16 02:39:15 +0530

    user registration backend

--- server/src/main/java/com/Cranco/Cranco/CrancoApplication.java
+++ server/src/main/java/com/Cranco/Cranco/CrancoApplication.java
@@ -1,9 +1,7 @@
 package com.Cranco.Cranco;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrancoApplication {
@@ -11,9 +9,5 @@ public class CrancoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrancoApplication.class, args);
 	}
-	@Bean
-	public ModelMapper modelMapper()
-	{
-		return new ModelMapper();
-	}
+
 }
