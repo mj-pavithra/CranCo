685a42bbdfccabe40e35b45f57e6938477d65b1c
Anjuna Serasingha <92202036+anjuna0305@users.noreply.github.com>
2023-08-20 15:41:56 +0530

    backend files added

---
+++ server/src/main/java/com/Cranco/Cranco/SecurityConfig/SecurityConfig.java
@@ -0,0 +1,14 @@
+//package com.Cranco.Cranco.SecurityConfig;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+//import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+////
+////@Configuration
+////@EnableWebSecurity
+////public class SecurityConfig extends WebSecurityConfiguration {
+////
+////}
---
+++ server/src/main/java/com/Cranco/Cranco/User/UserRepository.java
@@ -0,0 +1,11 @@
+package com.Cranco.Cranco.User;
+
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends Neo4jRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+}
