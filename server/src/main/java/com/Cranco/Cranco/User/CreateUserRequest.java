3f920756b1c95226041eff5ab23daa753b06a140
Anjuna Serasingha <92202036+anjuna0305@users.noreply.github.com>
2023-08-21 08:45:00 +0530

    controller now accept form-data

--- server/src/main/java/com/Cranco/Cranco/User/CreateUserRequest.java
+++ server/src/main/java/com/Cranco/Cranco/User/CreateUserRequest.java
@@ -8,12 +8,22 @@ public class CreateUserRequest {
     public String getUsername() {
         return username;
     }
-
     public String getPassword() {
         return password;
     }
-
     public String getEmail() {
         return email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
