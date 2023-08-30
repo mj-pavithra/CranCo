32d7746c6bb8816961df909b6af2c38efc1fa742
awed-00 <92774423+AmayaNW@users.noreply.github.com>
2023-08-30 12:20:44 +0530

    Vehicle Profile and Business Profile nodes.

---
+++ server/src/main/java/com/Cranco/Cranco/VehiclProfile/VehicleProfile.java
@@ -0,0 +1,120 @@
+package com.Cranco.Cranco.User;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Property;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Node("VEHICLEPROFILE")
+public class VehicleProfile {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Property("vehicle_name")
+    private String vehicleName;
+    @Property("prev_owners")
+    private String prevOwners;
+    @Property("gov_reg_no")
+    private String govRegNo;
+    @Property("vehicle_brand")
+    private String vehicleBrand;
+    @Property("vehicle_model")
+    private String vehicleModel;
+    @column(columnDefinition = "boolean default false")
+    @property("visibility")
+    private boolean visibility;
+    @column(columnDefinition = "boolean default false")
+    @property("ownership")
+    private boolean ownership;
+    @column(columnDefinition = "boolean default false")
+    @property("on_marketplace")
+    private boolean onMarketplace;
+
+    public VehicleProfile() {
+        this.visiblity = false;
+        this.ownership = false;
+        this.onMarketplace = false;
+    }
+
+// getters and setters
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getPrevOwners() {
+        return prevOwners;
+    }
+
+    public boolean isOnMarketplace() {
+        return onMarketplace;
+    }
+
+    public boolean isOwnership() {
+        return ownership;
+    }
+
+    public boolean isVisibility() {
+        return visibility;
+    }
+
+    public String getVehicleBrand() {
+        return vehicleBrand;
+    }
+
+    public String getVehicleModel() {
+        return vehicleModel;
+    }
+
+    public String getVehicleName() {
+        return vehicleName;
+    }
+
+    public String getGovRegNo() {
+        return govRegNo;
+    }
+
+    public void setGovRegNo(String govRegNo) {
+        this.govRegNo = govRegNo;
+    }
+
+    public void setOnMarketplace(boolean onMarketplace) {
+        this.onMarketplace = onMarketplace;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setOwnership(boolean ownership) {
+        this.ownership = ownership;
+    }
+
+    public void setVehicleBrand(String vehicleBrand) {
+        this.vehicleBrand = vehicleBrand;
+    }
+
+    public void setVehicleModel(String vehicleModel) {
+        this.vehicleModel = vehicleModel;
+    }
+
+    public void setVehicleName(String vehicleName) {
+        this.vehicleName = vehicleName;
+    }
+
+    public void setPrevOwners(String prevOwners) {
+        this.prevOwners = prevOwners;
+    }
+
+    public void setVisibility(boolean visibility) {
+        this.visibility = visibility;
+    }
+
+
+}
