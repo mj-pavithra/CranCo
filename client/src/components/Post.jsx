a5acba7a4130b0b901c395d3cf3a2cbebf3978cb
MJ Pavithra <mjpavithra6520@gmail.com>
2023-09-06 12:25:15 +0530

    images added in to the bite array but not fixed frontend

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -13,13 +13,25 @@ import Hr from "./Hr";
 import { Link } from "react-router-dom";
 import LinkToProfile from "../functions/LinkToProfile";
 
-const Post = (isOwner, username, caption, imageLocations
-  ) => {
-  const images = [
+const Post = ({
+  isOwner,
+  username,
+  caption,
+  imageLocations,
+  date,
+  time,
+}) => {
+  const postUsername = username || "Default Username";
+  const captionText = caption || "Default caption text";
+  const postDate = date || "January 1, 2023";
+  const postTime = time || "12:00 AM";
+
+  const fallbackImages = [
     "/assets/car_img_1.jpg",
     "/assets/car_img_2.jpg",
     "/assets/car_img_3.jpg",
   ];
+  const postImages = imageLocations || fallbackImages;
 
   const [liked, updateLiked] = useState(false);
 
@@ -34,21 +46,14 @@ const Post = (isOwner, username, caption, imageLocations
         <div className="post-back">
           <div className="post-header gap-3">
             <Link className="link-unstyled" to={LinkToProfile(isOwner)}>
-              <img
-                className="post-user-image"
-                src="/assets/profile.jpg"
-                alt=""
-              />
+              <img className="post-user-image" src="/assets/profile.jpg" alt="" />
             </Link>
             <div className="d-flex flex-column">
               <Link className="link-unstyled" to="/user">
-                <div className="fw-bold">
-                  {sessionStorage.getItem("username")
-                    ? sessionStorage.getItem("username")
-                    : "Sanduni Nimsara"}
-                </div>
+                <div className="fw-bold">{postUsername}</div>
               </Link>
-              <div className="post-time fw-light">just now</div>
+              <div className="post-time fw-light">{postDate} {postTime}</div>
+              <div className="post-time fw-light">{captionText}</div>
             </div>
           </div>
 
@@ -62,26 +67,19 @@ const Post = (isOwner, username, caption, imageLocations
           </div>
         </div>
 
-        {/* post cacpiton */}
         <div className="post-text post-padding pt-2 fw-semiBold"> </div>
 
         {/* image section */}
         <div className="post-image pt-2">
-          <Carousel images={images} />
+          <Carousel images={postImages} />
         </div>
 
         <div className="post-back pt-2">
-          <div
-            className={`post-header align-items-center gap-${
-              liked === true ? "2" : "1"
-            }`}
-          >
+          <div className={`post-header align-items-center gap-${liked === true ? "2" : "1"}`}>
             <div className="post-heart-div">
               <FontAwesomeIcon className="post-heart-icon" icon={faHeart} />
             </div>
-            <span className="fw-light txt-09">{`${
-              liked === true ? "You and " : ""
-            } 13 ${liked === true ? " others" : ""}`}</span>
+            <span className="fw-light txt-09">{`${liked === true ? "You and " : ""} 13 ${liked === true ? " others" : ""}`}</span>
           </div>
 
           <div className="post-header txt-09 gap-2">
@@ -96,14 +94,8 @@ const Post = (isOwner, username, caption, imageLocations
         </div>
 
         <div className="post-back pt-2">
-          <div
-            className="col-4 post-action-div gap-2"
-            onClick={() => handleLike()}
-          >
-            <FontAwesomeIcon
-              className={`post-action-icon ${liked === true ? "active" : ""}`}
-              icon={liked === true ? faHeart : reg_heart}
-            />
+          <div className="col-4 post-action-div gap-2" onClick={() => handleLike()}>
+            <FontAwesomeIcon className={`post-action-icon ${liked === true ? "active" : ""}`} icon={liked === true ? faHeart : reg_heart} />
             <div className="text-medium">Like</div>
           </div>
 
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,37 +1,50 @@
-import React from "react";
-import Post from "../components/Post"; // Import any other specific components for the Home page
+import React, { useState, useEffect } from "react";
+import Post from "../components/Post";
 import MainContainer from "../containers/MainContainer.jsx";
 import axios from "axios";
 
-
 const HomePage = () => {
+  const [postData, setPostData] = useState([]); // Define a state variable to hold the data
 
-  const loadFeed = async () => {
-    try {
-      const response = await axios.get('http://localhost:8081/api/posts/feed');
-      console.log("Data received:", response.data);
-    } catch (error) {
-      console.error("Error receiving data:", error);
-    }
-  };
+  useEffect(() => {
+    const loadFeed = async () => {
+      try {
+        const response = await axios.get('http://localhost:8081/api/posts/feed');
+        console.log("Data received:", response.data);
+        setPostData(response.data); // Set the received data in the state
+      } catch (error) {
+        console.error("Error receiving data:", error);
+      }
+    };
 
-  loadFeed();
+    loadFeed();
+  }, []);
 
   const imagelist = [
     "/assets/car_img_1.jpg",
     "/assets/car_img_2.jpg",
     "/assets/car_img_3.jpg",
   ];
-  if (sessionStorage.getItem("username") == "") {
+
+  if (sessionStorage.getItem("username") === "") {
     window.location.href = "http://localhost:3000/login";
   }
+
   return (
     <>
       <MainContainer>
-        <Post isOwner="no" images={imagelist} />
-        <Post isOwner="no" images={imagelist}/>
-        <Post isOwner="no" images={imagelist}/>
-        <Post isOwner="no" images={imagelist}/>
+        {/* Render Post components based on the data */}
+        {postData.map((post, index) => (
+          <Post
+            key={index}
+            isOwner="no"
+            username={post.username}
+            caption={post.caption}
+            imageLocations={post.imageLocations}
+            date={post.date}
+            time={post.time}
+          />
+        ))}
       </MainContainer>
     </>
   );
