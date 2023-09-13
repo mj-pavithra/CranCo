d1db747a5b215f502b4b961296c9911f4e41104e
Tharundya <wmtharu7899@gmail.com>
2023-09-13 14:27:53 +0530

    Notification Final

--- server/src/main/java/com/Cranco/Cranco/CrancoApplication.java
+++ server/src/main/java/com/Cranco/Cranco/CrancoApplication.java
@@ -3,9 +3,10 @@ package com.Cranco.Cranco;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class CrancoApplication {
 
 	public static void main(String[] args) {
--- server/src/main/java/com/Cranco/Cranco/Notification/NotificationRepository.java
+++ server/src/main/java/com/Cranco/Cranco/Notification/NotificationRepository.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import com.Cranco.Cranco.User.User;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NotificationRepository extends Neo4jRepository<Notification, Integer> {
     List<Notification> findByReceiverAndSeen(User receiver, boolean seen);
 
