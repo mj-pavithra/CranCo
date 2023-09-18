7f3560250d02ea0fa29b692bb7eeed04ce1ce36b
Suchith Sandunika <esuchith@gmail.com>
2023-09-18 07:22:07 +0530

    fixed the issues of the complaint functionality, did the CRUD and linked with the front end UI, created some jsx and css files related to this.

--- server/src/main/java/com/Cranco/Cranco/Complaint/ComplaintRepository.java
+++ server/src/main/java/com/Cranco/Cranco/Complaint/ComplaintRepository.java
@@ -1,9 +1,14 @@
 package com.Cranco.Cranco.Complaint;
 
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ComplaintRepository extends Neo4jRepository<Complaint, Long> {
-
+    List<Complaint> findByPostId(long postId);
+    @Query("MATCH (c:Report)\n" + "RETURN c;")
+    List<Complaint> findAllByOrderByPostIdASC();
 }
--- server/src/main/java/com/Cranco/Cranco/Complaint/ComplaintService.java
+++ server/src/main/java/com/Cranco/Cranco/Complaint/ComplaintService.java
@@ -3,81 +3,35 @@ package com.Cranco.Cranco.Complaint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ComplaintService {
 
-    private ComplaintRepository complaintRepository;
+    private final ComplaintRepository complaintRepository;
 
     @Autowired
     public ComplaintService(ComplaintRepository complaintRepository) {
         this.complaintRepository = complaintRepository;
     }
 
-    public Complaint createComplaint(ComplaintDTO complaintDTO) {
+    public Complaint complaintPost(ComplaintDTO complaintDTO){
         Complaint complaint = new Complaint();
-        complaint.settitle(complaintDTO.gettitle());
-        complaint.setdescription(complaintDTO.getdescription());
-        complaint.setIsClosed(true);
-        complaint.setIsReviewed(false);
+        complaint.setPostId(complaintDTO.getPostId());
+        complaint.setReason(complaintDTO.getReason());
         return complaintRepository.save(complaint);
     }
 
-    public Complaint updateComplaint(Long id, ComplaintDTO complaintDTO) {
-        Complaint complaint = complaintRepository.findById(id).orElse(null);
-        if (complaint != null) {
-            complaint.settitle(complaintDTO.gettitle());
-            complaint.setdescription(complaintDTO.getdescription());
-            return complaintRepository.save(complaint);
-        }
-        return null;
+    public List<Complaint> getAllReportsForPost(long postId) {
+        return complaintRepository.findByPostId(postId);
     }
 
-    public void reviewComplaint(Long id) {
-        Complaint complaint = complaintRepository.findById(id).orElse(null);
-        if (complaint != null) {
-            complaint.setIsReviewed(true);
-            complaintRepository.save(complaint);
-        }
+    public List<Complaint> getAllComplaintsSortedByPostId() {
+        return complaintRepository.findAllByOrderByPostIdASC();
     }
 
-    public void closeComplaint(Long id) {
-        Complaint complaint = complaintRepository.findById(id).orElse(null);
-        if (complaint != null && complaint.isReviewed()) {
-            complaint.setIsClosed(false);
-            complaintRepository.save(complaint);
-        }
-    }
-
-    public void deleteComplaint(Long id) {
-        complaintRepository.deleteById(id);
+    public void deleteComplaint(Long reportId) {
+        complaintRepository.deleteById(reportId);
     }
 }
-
-//    public List<Complaint> getAllComplaints() {
-//        return (List<Complaint>) complaintRepository.findAll();
-//    }
-
-//    public ComplaintDTO maptoDTO(Complaint complaint) {
-//        ComplaintDTO dto = new ComplaintDTO();
-//        dto.setComplaint_id(complaint.getComplaint_id());
-//        dto.setComplaint_title(complaint.getComplaint_title());
-//        dto.setComplaint_description(complaint.getComplaint_description());
-//        dto.setIsClosed(complaint.isClosed());
-//        dto.setIsReviewed(complaint.isReviewed());
-//        return dto;
-//    }
-
-
-//
-//    public List<Complaint> getReviewedComplaints() {
-//        return complaintRepository.findByIsReviewed(true);
-//    }
-//
-//    public List<Complaint> getClosedComplaints() {
-//        return complaintRepository.findByIsOpen(false);
-//    }
-//
-//    public Complaint getComplaintById(Long complaintId) {
-//        return complaintRepository.findById(complaintId).orElse(null);
-//    }
 
