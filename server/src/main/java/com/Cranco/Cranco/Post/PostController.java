e1866eaeef0e882de7259e55914ff1920b8ec4e0
Suchith Sandunika <esuchith@gmail.com>
2023-09-29 13:58:28 +0530

    linked frontend and backend related to post management. Fixed some errors.

--- server/src/main/java/com/Cranco/Cranco/Post/PostController.java
+++ server/src/main/java/com/Cranco/Cranco/Post/PostController.java
@@ -1,8 +1,11 @@
 package com.Cranco.Cranco.Post;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -16,5 +19,35 @@ public class PostController {
     public ResponseEntity<PostDto> createPost(@RequestBody CreatePost request){
         PostDto createPost = postService.createPost(request);
         return ResponseEntity.ok(createPost);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Post>> getAllPostsSortedByPostId() {
+        try {
+            List<Post> postList = postService.getAllPostsSortedByPostId();
+            return ResponseEntity.ok(postList);
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+//    @PutMapping("/{postId}")
+//    public ResponseEntity<Post> updatePostDetails(@PathVariable Long postId, @RequestBody PostDto postDto) {
+//        try {
+//            Post Post = postService.updatePostdetails(postId, postDto);
+//            return ResponseEntity.ok(post);
+//        } catch (Exception ex) {
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+//        }
+//    }
+
+    @DeleteMapping("{postId}")
+    public ResponseEntity<Post> deletePost(@PathVariable Long postId) {
+        try {
+            postService.deletePost(postId);
+            return ResponseEntity.ok().build();
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
