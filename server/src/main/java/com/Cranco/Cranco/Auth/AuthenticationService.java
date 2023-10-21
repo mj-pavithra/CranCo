00e32cc7d1cf9cfc00175efaf77f92794f33fe71
Anjuna Serasingha <anjunaserasingha@gmail.com>
2023-10-21 12:45:33 +0530

    still working

--- server/src/main/java/com/Cranco/Cranco/Auth/AuthenticationService.java
+++ server/src/main/java/com/Cranco/Cranco/Auth/AuthenticationService.java
@@ -6,6 +6,7 @@ import com.Cranco.Cranco.User.User;
 import com.Cranco.Cranco.User.UserRepository;
 import com.Cranco.Cranco.Config.JwtService;
 import lombok.RequiredArgsConstructor;
+import org.jooq.meta.derby.sys.Sys;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,45 +17,56 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
-  private final UserRepository repository;
-  private final PasswordEncoder passwordEncoder;
-  private final JwtService jwtService;
-  private final AuthenticationManager authenticationManager;
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
-  public AuthenticationResponse register(RegisterRequest request) {
+    public AuthenticationResponse register(RegisterRequest request) {
 
-    Optional<User> userByEmail = repository.findByEmail(request.getEmail());
-    if (!userByEmail.isEmpty()) {
-      throw new IllegalStateException(("email taken"));
+        Optional<User> userByEmail = repository.findByEmail(request.getEmail());
+        if (!userByEmail.isEmpty()) {
+            throw new IllegalStateException(("email taken"));
+        }
+        var user = User.builder().username(request.getUsername()).email(request.getEmail()).password(passwordEncoder.encode(request.getPassword())).role(Role.USER).build();
+        repository.save(user);
+        System.out.println("user added " + request.getUsername());
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwtToken).build();
     }
-    var user = User.builder()
-            .username(request.getUsername())
-            .email(request.getEmail())
-            .password(passwordEncoder.encode(request.getPassword()))
-            .role(Role.USER)
-            .build();
-    repository.save(user);
-    System.out.println("user added " + request.getUsername());
-    var jwtToken = jwtService.generateToken(user);
-    return AuthenticationResponse.builder()
-            .token(jwtToken)
-            .build();
-  }
 
-  public AuthenticationResponse authenticate(AuthenticationRequest request) {
-    System.out.println(request);
-    authenticationManager.authenticate(
-        new UsernamePasswordAuthenticationToken(
-            request.getUsername(),
-            request.getPassword()
-        )
-    );
-    var user = repository.findByEmail(request.getUsername())
-        .orElseThrow();
-    user.setUsername(user.getEmail());
-    var jwtToken = jwtService.generateToken(user);
-    return AuthenticationResponse.builder()
-        .token(jwtToken)
-        .build();
-  }
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        var user = repository.findByEmail(request.getUsername()).orElseThrow();
+        user.setUsername(user.getEmail());
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwtToken).build();
+    }
+
+//    public void changePassword(
+//            String userEmail,
+//            String currentPassword,
+//            String newPassword
+//    ) {
+//    }
+
+
+    public void changePassword(
+            String userEmail,
+            String currentPassword,
+            String newPassword
+    ) throws Exception {
+        System.out.println("service called");
+        if (currentPassword == null || newPassword == null || userEmail == null) {
+            System.out.println("forst if called");
+            throw new Exception("bad request");
+        }
+        System.out.println("forst if passed");
+        var user = repository.findByEmail(userEmail).orElseThrow();
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userEmail, currentPassword));
+        System.out.println("password matched");
+        user.setPassword(passwordEncoder.encode(newPassword));
+        repository.save(user);
+        System.out.println("password changed");
+    }
 }
